8a01c0339be5a392a632f1537b75915df002ca65
Alexuber <alexuberbuber@gmail.com>
2023-03-21 13:42:33 +0200

    add layoutAuth

---
+++ src/components/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+const AuthLayout = ({ children }) => {
+  //   useEffect(() => {
+  //     dispatch(current());
+  //   }, []);
+
+  return children;
+};
+
+export default AuthLayout;
